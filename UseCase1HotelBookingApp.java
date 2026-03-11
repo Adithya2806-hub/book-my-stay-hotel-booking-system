c78570d2b270beb3e6b3f5928ded0a3a02a3360c
Adithya2806-huB <tr4112@srmist.edu.in>
2026-03-11 08:58:27 +0530

    Base application setup

---
+++ UseCase1HotelBookingApp.java
@@ -0,0 +1,21 @@
+/**
+ * UseCase1HotelBookingApp
+ *
+ * Entry point of Book My Stay Hotel Booking System
+ *
+ * @author Adithya
+ * @version 1.0
+ */
+
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to Book My Stay ");
+        System.out.println(" Hotel Booking System v1.0 ");
+        System.out.println("=================================");
+
+        System.out.println("Application Started Successfully!");
+    }
+}
